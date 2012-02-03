5dfe4cefda813fd8e8f7607261dbff5baae32534
Justin DiPierro <dipierroj@gmail.com>
2012-02-02 19:47:44 -0500

    Finished the Zone header but not the source.

--- world/Zone.h
+++ world/Zone.h
@@ -1,1 +1,33 @@
-#include "Tile.h"
+#ifndef _CAREA_H_
+  #define _CAREA_H_
+
+#include "Level.h"
+
+class Zone
+{
+public:
+    static Zone		ZoneControl;
+    
+public:
+    std::vector<Level>	LevelList;
+    
+private:
+    int			ZoneSize;
+    
+    SDL_Surface*	Surf_Tileset;
+    
+public:
+    Zone();
+    
+    bool	OnLoad(char* file);
+    
+    void	OnRender(SDL_Surface* Surf_Display, int CameraX, int CameraY);
+    
+    void	OnCleanup();
+    
+public:
+    Level*	GetLevel(int X, int Y);
+    Tile*	GetTile(int X, int Y);
+};
+
+#endif
