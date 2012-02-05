0db0f246281d2df59287f9a2fe6f71fd44534693
Justin DiPierro <dipierroj@gmail.com>
2012-02-04 19:25:56 -0500

    Recursively delete all the entities on cleanup

--- app/App_OnCleanup.cpp
+++ app/App_OnCleanup.cpp
@@ -5,6 +5,14 @@ void App::OnCleanup()
 
 	if(Surf_Test != NULL)
 		SDL_FreeSurface(Surf_Test);
+	
+	for(int i = i; i < Entity::EntityList.size(); i++)
+	{
+	    if(Entity::EntityList[i])
+	    {
+		delete Entity::EntityList[i];
+	    }
+	}
 
 	SDL_Quit();
 	std::cout << "Cleanup Complete." << std::endl;
